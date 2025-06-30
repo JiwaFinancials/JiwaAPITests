73ff63e1459d49b2730b297bee507601c4964f36
Mike Sheen <mike@sheen.id.au>
2025-06-30 09:37:40 +0800

    Inventory Classification CRUD

---
+++ Inventory/Classification.cs
@@ -0,0 +1,60 @@
+﻿using JiwaFinancials.Jiwa.JiwaServiceModel.Inventory;
+using JiwaFinancials.Jiwa.JiwaServiceModel;
+using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JiwaFinancials.Jiwa.JiwaServiceModel.Inventory.Classification;
+
+namespace JiwaAPITests.Inventory
+{
+    public class Classification : JiwaAPITest
+    {
+        #region "{Main}"
+        [Test]
+        public async Task InventoryClassification_CRUD()
+        {
+            // Create an inventory classification
+            InventoryClassificationPOSTRequest classificationCreateReq = new InventoryClassificationPOSTRequest()
+            {
+                Description = RandomString(5)
+            };
+
+            InventoryClassification classificationCreateRes = await Client.PostAsync(classificationCreateReq);
+            Assert.That(LastHttpStatusCode, Is.EqualTo(System.Net.HttpStatusCode.Created));
+            Assert.That(classificationCreateRes.Description, Is.EqualTo(classificationCreateReq.Description));
+            Assert.That(classificationCreateRes.ClassificationID, !Is.Null);
+
+            // Read the created item using the ClassificationID
+            InventoryClassificationGETRequest classificationGetReq = new InventoryClassificationGETRequest() { ClassificationID = classificationCreateRes.ClassificationID };
+            InventoryClassification classificationGetRes = await Client.GetAsync(classificationGetReq);
+            Assert.That(LastHttpStatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+            Assert.That(classificationGetRes.Description, Is.EqualTo(classificationCreateReq.Description));
+
+            // Update the classification
+            InventoryClassificationPATCHRequest classificationPatchReq = new InventoryClassificationPATCHRequest()
+            {
+                ClassificationID = classificationCreateRes.ClassificationID,
+                Description = RandomString(5)
+            };
+            InventoryClassification classificationPatchRes = await Client.PatchAsync(classificationPatchReq);
+            Assert.That(LastHttpStatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+            Assert.That(classificationPatchRes.Description, Is.EqualTo(classificationPatchReq.Description));
+
+            // Remove the created classification
+            InventoryClassificationDELETERequest classificationDELETEReq = new InventoryClassificationDELETERequest() { ClassificationID = classificationCreateRes.ClassificationID };
+            await Client.DeleteAsync(classificationDELETEReq);
+            Assert.That(LastHttpStatusCode, Is.EqualTo(System.Net.HttpStatusCode.NoContent));
+
+            // ensure the deleted item is not there anymore            
+            WebServiceException ex = Assert.ThrowsAsync<ServiceStack.WebServiceException>(async () =>
+            {
+                InventoryClassification getDeletedRes = await Client.GetAsync(classificationGetReq);
+            });
+            Assert.That(ex.StatusCode, Is.EqualTo(404));            
+        }
+        #endregion
+    }
+}
